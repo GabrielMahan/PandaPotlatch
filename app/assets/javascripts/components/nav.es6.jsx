f9ecef413c9f92c1d8de3dbc354a332f1a423eee
Myra Orgain <email": null>
2016-08-29 14:42:43 -0400

    Create nav bar react component

---
+++ app/assets/javascripts/components/nav.es6.jsx
@@ -0,0 +1,14 @@
+class Nav extends React.Component  {
+  render () {
+    return (
+      <div className="NavBar">
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
