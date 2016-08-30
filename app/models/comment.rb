ea7201af8ba34d4864b3f3f3e857d352a5e36f54
Myra Orgain <email": null>
2016-08-30 15:00:39 -0400

    Create vote seeds

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,6 @@
 class Comment < ApplicationRecord
 
 belongs_to :commentable, polymorphic: true
-belongs_to :movie
-belongs_to :review
-belongs_to :user
 has_many :votes, as: :voteable
 
 validates :body, :user_id, :commentable_type, :commentable_id, presence: true
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,5 @@
 class Vote < ApplicationRecord
   belongs_to :voteable, polymorphic: true
-  belongs_to :movie
-  belongs_to :comment
-  belongs_to :review
 
   validates :user_id, :voteable_type, :voteable_id, presence: true
 end
