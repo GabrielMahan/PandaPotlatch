cc0c90be97193884e33c06bd11630aaf3333915a
Gabriel Mahan & Myra Orgain & Nicholas Wang <pair+gabrielmahan.mlorgain.nwang9@devbootcamp.com>
2016-08-30 18:49:52 -0400

    made changes

--- app/assets/javascripts/components/ReviewList.es6.jsx
+++ app/assets/javascripts/components/ReviewList.es6.jsx
@@ -1,24 +1,11 @@
 class ReviewList extends React.Component {
 
-
-  constructor() {
-    super();
-    this.state = {
-                    reviews: this.props.reviews,
-                    movie: this.props.movie
-                 }
-  }
-
-
-
-
   render() {
     return (
       <div>
         this is review list
         <ul>
-
-          { this.props.reviews.map(function(review) {
+          {this.props.reviews.map(function(review) {
             return <li><a href={`/movies/${movie.id}/reviews/${review.id}`}> review.title </a></li>
           })}
         </ul>
