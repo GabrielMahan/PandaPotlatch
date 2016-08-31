602a2ba197ee87d6fbccb07c07c609d5666fdd06
Nicholas Wang <nicowang9@gmail.com>
2016-08-31 15:29:40 -0400

    can only vot eonce DID IT BOYS

--- app/assets/javascripts/components/review.es6.jsx
+++ app/assets/javascripts/components/review.es6.jsx
@@ -7,6 +7,7 @@ class Review extends React.Component {
   render() {
     return (
       <div>
+        <VoteButton review={this.props.review} />
         <h1> {JSON.parse(this.props.review).title} </h1>
         <p> Score: {JSON.parse(this.props.review).tomato_score}</p>
         <p>{JSON.parse(this.props.review).body}</p>
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,59 +5,83 @@ before_action :data, only:[:index]
   def index
     if params[:movie]
       find_movie
-      value = vote_value(@movie)
+      value = vote_value(@object)
     elsif params[:comment]
       find_comment
-      value = vote_value(@comment)
+      value = vote_value(@object)
     else params[:review]
       find_review
-      value = vote_value(@review)
+      value = vote_value(@object)
     end
       return render json: value
   end
 
   def upvote
     if params[:movie]
-      typeobj = params[:movie]
       find_movie
-      type = "Movie"
-      create_upvote(type, typeobj)
-      value = vote_value(@movie)
+      if find_vote(@object)
+        value = vote_value(@object)
+      else
+        typeobj = params[:movie]
+        type = "Movie"
+        create_upvote(type, typeobj)
+        value = vote_value(@object)
+      end
     elsif params[:comment]
-      typeobj = params[:comment]
       find_comment
-      type = "Comment"
-      create_upvote(type, typeobj)
-      value = vote_value(@comment)
+      if find_vote(@object)
+        value = vote_value(@object)
+      else
+        typeobj = params[:comment]
+        type = "Comment"
+        create_upvote(type, typeobj)
+        value = vote_value(@object)
+      end
     else params[:review]
-      typeobj = params[:review]
       find_review
-      type = "Review"
-      create_upvote(type, typeobj)
-      value = vote_value(@review)
+      if find_vote(@object)
+        value = vote_value(@object)
+      else
+        typeobj = params[:review]
+        type = "Review"
+        create_upvote(type, typeobj)
+        value = vote_value(@object)
+      end
     end
       return render json: value
   end
 
   def downvote
     if params[:movie]
-      typeobj = params[:movie]
-      type = "Movie"
-      create_downvote(type, typeobj)
       find_movie
-      value = vote_value(@movie)
+      if find_vote(@object)
+         value = vote_value(@object)
+      else
+          typeobj = params[:movie]
+          type = "Movie"
+          create_downvote(type, typeobj)
+          value = vote_value(@object)
+      end
     elsif params[:comment]
       find_comment
-      type = "Comment"
-      typeobj = params[:comment]
-      create_downvote(type,typeobj)
-      value = vote_value(@comment)
+      if find_vote(@object)
+        value = vote_value(@object)
+      else
+        type = "Comment"
+        typeobj = params[:comment]
+        create_downvote(type,typeobj)
+        value = vote_value(@object)
+      end
     else params[:review]
       find_review
-      type = "Review"
-      typeobj = params[:review]
-      create_downvote(type, typeobj)
-      value = vote_value(@review)
+      if find_vote(@object)
+        value = vote_value(@object)
+      else
+        type = "Review"
+        typeobj = params[:review]
+        create_downvote(type, typeobj)
+        value = vote_value(@object)
+      end
     end
       return render json: value
   end
@@ -79,15 +103,15 @@ private
   end
 
   def find_movie
-    @movie = Movie.find(params[:movie][:id])
+    @object = Movie.find(params[:movie][:id])
   end
 
   def find_comment
-    @comment = Comment.find(params[:comment][:id])
+    @object = Comment.find(params[:comment][:id])
   end
 
   def find_review
-    @review = Review.find(params[:review][:id])
+    @object = Review.find(params[:review][:id])
   end
 
   def create_upvote(type, typeobj)
@@ -97,4 +121,11 @@ private
   def create_downvote(type, typeobj)
     Vote.create(user_id: current_user.id, up?: false, voteable_type: type, voteable_id: typeobj[:id])
   end
+
+  def find_vote(object)
+    votes = object.votes
+    return true if votes.find_by(user_id: current_user.id)
+  end
+
+
 end
